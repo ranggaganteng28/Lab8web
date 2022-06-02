e5cd1f3c0929f1be0efc31f5a8729704de4c3819
ranggaganteng28 <ranggasaputra.rr@gmail.com>
2022-06-02 16:41:56 +0700

    praktikum8

--- lab8_php_database/koneksi.php
+++ lab8_php_database/koneksi.php
@@ -1,12 +1,3 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>koneksi</title>
-</head>
-<body>
 <?php
 $host = "localhost";
 $user = "root";
@@ -18,7 +9,5 @@ if ($conn == false)
 {
     echo "Koneksi ke server gagal.";
     die();
-} else echo "Koneksi berhasil";
+} //else echo "Koneksi berhasil";
 ?>
-</body>
-</html>
